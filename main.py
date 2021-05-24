679a8ded50f79c3959daec4596ac1ceabce39ca6
Michelle282 <michelleadunne@gmail.com>
2021-05-24 21:08:48 +0100

    Updated my repository with new csv file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 #Import a CSV File into a Pandas DataFrame
+Premier_League_FF = pd.read_csv("Premier League - Football and Financial Performance - 2017 - 2020.csv")
+print(Premier_League_FF.head())
 
-nyc_house_prices = pd.read_csv('nyc-rolling-sales.csv')
-print(nyc_house_prices.head())
