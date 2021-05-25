e4788d3a89f9de2a53e94ed3064dad265887b539
Michelle282 <michelleadunne@gmail.com>
2021-05-25 21:24:48 +0100

    Grouped and dropped duplicates from DF, added csv file output

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ Premier_League_FF = pd.read_csv("Premier League - Football and Financial Perform
 Premier_League_FF.sort_values(["Year", "Finishing Position"], axis=0,
                  ascending=True, inplace=True)
 #print(Premier_League_FF.head())
-Premier_League_FF.to_csv('PL_FF1.csv',index=True)
+#Premier_League_FF.to_csv('PL_FF1.csv',index=True)
 
 #Index DF to see first 8 columns for all teams in 2017
 Indexed = Premier_League_FF.iloc[0:17,0:8]
-print(Indexed.head())
+#print(Indexed.head())
+
+# Get the last entry for each team
+Grouped=Premier_League_FF.groupby('Team').last()
+#print(Grouped)
+#Grouped.to_csv(r'Grouped_PLFF.csv')
+
+#Drop Duplicates on Teams Column
+Drop_Duplicates=Premier_League_FF.drop_duplicates(subset=['Team'])
+#print(Drop_Duplicates)
