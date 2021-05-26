ce05ec56f6197c9d5d38d8a28b5c90ab91be3b05
Michelle282 <michelleadunne@gmail.com>
2021-05-26 21:52:46 +0100

    Create 2 dataframes to perform merge

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Premier_League_FF = pd.read_csv("Premier League - Football and Financial Perform
 # print(Premier_League_FF.head())
 
 # Sort DF by Columns - Year and Finishing Position
-Premier_League_FF.sort_values(["Year", "Finishing Position"], axis=0,
-                              ascending=True, inplace=True)
-# print(Premier_League_FF.head())
-# Premier_League_FF.to_csv('PL_FF1.csv',index=True)
+#Premier_League_FF.sort_values(["Year", "Finishing Position"], axis=0,
+                              #ascending=True, inplace=True)
+ #print(Premier_League_FF.head())
+ #Premier_League_FF.to_csv('PL_FF1.csv',index=True)
 
 # Index DF to see first 8 columns for all teams in 2017
 Indexed = Premier_League_FF.iloc[0:17, 0:8]
@@ -33,6 +33,14 @@ df_points = pd.DataFrame({
 	'Points': [75, 93, 76, 78, 69]})
 
 #iterate through each row of dataframe
-for index, row in df_points.iterrows():
-    print(index, ': ', row['Team'], 'has', row['Points'], 'Points.')
+#for index, row in df_points.iterrows():
+    #print(index, ': ', row['Team'], 'has', row['Points'], 'Points.')
 
+#Merge Dataframes - Create 2 DataFrame and Merge
+df2020 = {'Team':['Liverpool', 'Manchester City', 'Chelsea', 'Manchester United', 'Leicester City'], 'Year':['2020', '2020', '2020', '2020', '2020'], 'Points':['99', '81', '66', '66', '62'], 'Est Total Salary':['118m', '135m', '138m', '183m', '79m'], 'PoundsSpentPerPoint':['1.19m', '1.69m', '2.1m', '2.77m', '1.28m']}
+df1=pd.DataFrame(df2020, columns=['Team', 'Year', 'Points', 'Est Total Salary', 'PoundsSpentPerPoint'])
+#print(df1.head())
+
+df2019 = {'Team':['Manchester City', 'Liverpool', 'Chelsea', 'Manchester United', 'Leicester City'], 'Year':['2019', '2019', '2019', '2019', '2019'], 'Points':['98', '97', '72', '66', '52'], 'Est Total Salary':['146m', '115m', '121m', '158m', '75m'], 'PoundsSpentPerPoint':['1.49m', '1.19m', '1.68m', '2.38m', '1.45m']}
+df2=pd.DataFrame(df2019, columns=['Team', 'Year', 'Points', 'Est Total Salary', 'PoundsSpentPerPoint'])
+print(df2.head())
