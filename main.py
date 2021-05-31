a15306626d30f0893c3a101fee940d642d70b69e
Michelle282 <michelleadunne@gmail.com>
2021-05-31 17:20:59 +0100

    created csv output for sorted dataframe

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ Premier_League_FF = pd.read_csv("Premier League - Football and Financial Perform
 # print(Premier_League_FF.head())
 
 # Sort DF by Columns - Year and Finishing Position
-#Premier_League_FF.sort_values(["Year", "Finishing Position"], axis=0,
-                              #ascending=True, inplace=True)
- #print(Premier_League_FF.head())
- #Premier_League_FF.to_csv('PL_FF1.csv',index=True)
+Premier_League_FF.sort_values(["Year", "Finishing Position"],
+                              ascending=[False, True], inplace=True)
+print(Premier_League_FF.head())
+Premier_League_FF.to_csv('PL_Sorted.csv',index=True)
 
-# Index DF to see first 8 columns for all teams in 2017
-Indexed = Premier_League_FF.iloc[0:17, 0:8]
-# print(Indexed.head())
+# Index DF to see first 8 columns for all teams in
+Premier_League_FF = pd.read_csv("Premier League - Football and Financial Performance - 2017 - 2020.csv")
+Indexed = Premier_League_FF.iloc[:, :8]
+print(Indexed)
+Indexed.to_csv('IndexedPLFF.csv')
 
 # Get the last entry for each team
 Grouped = Premier_League_FF.groupby('Team').last()
